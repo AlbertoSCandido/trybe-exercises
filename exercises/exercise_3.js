2a8e38b97cd2ccaff0a65fca325510a21b5a01fd
Alberto Candido <albertocandido98@gmail.com>
2021-03-28 16:05:43 -0300

    Exercicio 3 - retorna maior de tres numeros

---
+++ exercises/exercise_3.js
@@ -0,0 +1,11 @@
+let a = 5789;
+let b = 1296;
+let c = 329746;
+
+if (a > b && a > c) {
+    console.log('O numero maior é: ' + a);
+} else if (b > a && b > c) {
+    console.log('O numero maior é: ' + b);
+} else {
+    console.log('O numero maior é: ' + c);
+}

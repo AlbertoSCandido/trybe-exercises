933569c1dc562262494b91d1b1d791985158a909
Alberto Candido <albertocandido98@gmail.com>
2021-03-29 16:15:33 -0300

    Exercicio 1 - dado um objeto info imprimir mensagem usando propriedades do objeto

---
+++ exercises/bloco_4/dia_4/materia.js
@@ -0,0 +1,63 @@
+// let car = {
+//     type: 'Fiat',
+//     model: '500',
+//     color: 'white',
+//   };
+
+// let player = {
+//   name: 'Marta',
+//   lastName: 'Silva',
+//   age: 34,
+//   medals: { 
+//     golden: 2,
+//     silver: 3 
+//   }
+// }
+
+// console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade');
+
+// player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
+
+// console.log( 'A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes nos anos: ' + player.bestInTheWorld);
+
+// console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata');
+
+// let cars = ['Saab', 'Volvo', 'BMW'];
+
+// for (let index in cars) {
+//   console.log(index, cars[index]);
+// }
+
+// let car = {
+//     type: 'Fiat',
+//     model: '500',
+//     color: 'white',
+//   };
+  
+//   for (let index in car) {
+//     console.log(index, car[index]);
+//   }
+
+
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge' 
+//   } 
+
+// for (let key in names) {
+//     console.log('Olá ' + names[key]);
+// }
+
+
+let carro = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  }
+
+for (let key in carro) {
+   console.log(key,':', carro[key]);
+}
+
+

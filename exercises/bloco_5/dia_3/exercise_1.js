4c7f335bcdc1ca555eea205f4bd34a925b481976
Alberto Candido <albertocandido98@gmail.com>
2021-04-12 10:28:59 -0300

    Exercicios 8 e 9

--- exercises/bloco_5/dia_3/exercise_1.js
+++ exercises/bloco_5/dia_3/exercise_1.js
@@ -1,3 +1,5 @@
+colorDiv = 'magenta'
+
 // 1
 // O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como filhos/filhas da tag <ul> com ID "days" . Note que os dias 29 e 30 de novembro estão no array pois representam respectivamente Segunda-feira e Terça-feira.
 // Os dias devem estar contidos em uma tag <li> , e todos devem ter a classe day . Ex: <li class="day">3</li>
@@ -139,19 +141,198 @@ createZoom()
 // O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
 
 
-function addTask() {
-  let buttonTask = document.getElementById('btn-add');
-  buttonTask.addEventListener('click', function(){
-  let inputBox = document.getElementById('task-input').value;
+function addTask(task) {
   let newTask = document.createElement('span');
-  newTask.style.display = 'block';
-  newTask.innerText = ` ${inputBox}`;
+  newTask.innerText = task;
   document.querySelector('.my-tasks').appendChild(newTask); 
+}
+
+addTask('Xablau: ');
+
+// 8
+// Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task .
+// O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+// O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
+
+
+function addColor(color) {
+  let newDiv = document.createElement('div');
+  newDiv.className = 'task';
+  newDiv.style.backgroundColor = color;
+  let parent = document.querySelector('.my-tasks');
+  parent.appendChild(newDiv);
+}
+
+addColor(colorDiv);
+
+
+//9
+// Implemente uma função que adiciona um evento que ao clicar no elemento com a tag <div> referente a cor da sua tarefa, atribua a este elemento a classe task selected , ou seja, quando sua tarefa possuir a classe task selected ela estará selecionada.
+// Ao clicar novamente no elemento a sua classe deverá voltar a ser somente task , ou seja, esta tarefa está deixando de ser uma tarefa selecionada.
+
+
+
+function addClassTask() {
+  let taskDiv = document.querySelector('.task');
+  taskDiv.addEventListener('click', function(){
+    if (taskDiv.className === 'task selected') {
+      taskDiv.className = 'task';
+    } else {
+      taskDiv.className = 'task selected';
+    }
   })
 }
 
-addTask()
+addClassTask();
+
+//10
+// Implemente uma função que adiciona um evento que ao clicar em um dia do mês no calendário, atribua a este dia a cor da legenda da sua tarefa selecionada.
+// Ao clicar novamente no dia com a cor da legenda, a sua cor deverá voltar à configuração inicial rgb(119,119,119) .
+
+
+
+function addTaskDay() {
+  let daysMonth = document.querySelectorAll('.day');
+  console.log(daysMonth);
+  let toggleColor = document.getElementsByClassName('task selected')
+  for(i = 0; i < daysMonth.length; i += 1) {
+  daysMonth[i].addEventListener('click', function(event){
+    if (toggleColor.length === 1) {
+      event.target.style.color = colorDiv;
+    } else {
+      event.target.style.color = 'rgb(119,119,119)'
+    }
+  })
+  }
+}
+
+
+addTaskDay()
 
 
 
 
+
+
+
+
+
+
+
+
+
+// function addTaskClass() {
+//   let selectedTask = document.querySelectorAll('.task .selected')
+//   let myTask = document.querySelector('.task');
+
+//   myTask.addEventListener('click', function (event) {
+//     if (selectedTask.length === 0) {
+//       event.target.className = 'task selected';
+//     } else {
+//       event.target.className = 'task';
+//     }
+//   })
+  
+// }
+
+// addTaskClass()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function setTaskClass() {
+//   let selectedTask = document.getElementsByClassName('task selected');
+//   let myTasks = document.querySelector('.task');
+
+//   myTasks.addEventListener('click', function(event) {
+//     if (selectedTask.length === 0) {
+//       event.target.className = 'task selected';
+//     } else {
+//       event.target.className = 'task';
+//     }
+//   });
+// };
+
+// setTaskClass();
+
+// 10 
+// Implemente uma função que adiciona um evento que ao clicar em um dia do mês no calendário, atribua a este dia a cor da legenda da sua tarefa selecionada.
+// Ao clicar novamente no dia com a cor da legenda, a sua cor deverá voltar à configuração inicial rgb(119,119,119) .
+
+// function setDayColor() {
+//   let selectedTask = document.getElementsByClassName('task selected');
+//   let days = document.querySelector('#days');
+//   let taskDiv = document.querySelector('.task');
+//   let taskColor = taskDiv.style.backgroundColor;
+  
+//   days.addEventListener('click', function(event){
+//     let eventTargetColor = event.target.style.color;
+//     if (selectedTask.length > 0 && eventTargetColor !== taskColor) {
+//       let color = selectedTask[0].style.backgroundColor;
+//       event.target.style.color = color;
+//     } else if (eventTargetColor === taskColor && selectedTask.length !== 0) {
+//       event.target.style.color = 'rgb(119,119,119)';
+//     }
+//   });
+// };
+
+// setDayColor();
+
+
+// Bonus
+
+// Vamos adicionar compromissos ao seu calendário? Implemente uma função que, ao digitar um compromisso na caixa de texto "COMPROMISSOS", adiciona o item à lista "MEUS COMPROMISSOS" ao clicar no botão "ADICIONAR".
+// Se nenhum caractere for inserido no campo input , a função deve retornar um alert com uma mensagem de erro ao clicar em "ADICIONAR".
+// Ao pressionar a tecla "enter" o evento também deverá ser disparado.
+
+// function addNewTask() {
+//   let getInputField = document.querySelector('#task-input');
+//   let addInputButton = document.querySelector('#btn-add');
+//   let getTaskList = document.querySelector('.task-list');
+
+//   addInputButton.addEventListener('click', function() {
+//     if (getInputField.value.length > 0) {
+//       let newLi = document.createElement('li');
+//       newLi.innerText = getInputField.value;
+
+//       getTaskList.appendChild(newLi);
+//       getInputField.value = '';
+//     } else {
+//       alert('Error: Digite ao menos 1 caractere.');
+//     }
+//   })
+
+//   getInputField.addEventListener('keyup', function(event) {
+//     if (event.keyCode === 13 && getInputField.value.length > 0) {
+//       let newLi = document.createElement('li');
+//       newLi.innerText = getInputField.value;
+
+//       getTaskList.appendChild(newLi);
+//       getInputField.value = '';
+//     }
+//   });
+// };
+
+// addNewTask();

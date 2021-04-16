e45de8cc7640ce0b382c289673e18cf2c26af5fc
Alberto Candido <albertocandido98@gmail.com>
2021-04-15 22:10:03 -0300

    Inicia formulario preenchendo todas as tags requizitadas em HTML e preenchimento dos Estados usando JS

---
+++ exercises/bloco_6/dia_1/script.js
@@ -0,0 +1,42 @@
+let states = document.querySelector('#brasil-states');
+let statsList = {
+  ac: 'Acre',
+  al: 'Alagoas',
+  ap: 'Amapá',
+  am: 'Amazonas',
+  ba: 'Bahia',
+  ce: 'Ceará',
+  df: 'Distrito Federal',
+  es: 'Espírito Santo',
+  go: 'Goiás',
+  ma: 'Maranhão',
+  mt: 'Mato Grosso',
+  ms: 'Mato Grosso do Sul',
+  mg: 'Minas Gerais',
+  pa: 'Pará',
+  pb: 'Paraíba',
+  pr: 'Paraná',
+  pe: 'Pernambuco',
+  pi: 'Piauí',
+  rr: 'Roraima',
+  ro: 'Rondônia',
+  rj: 'Rio de Janeiro',
+  rn: 'Rio Grande do Norte',
+  rs: 'Rio Grande do Sul',
+  sc: 'Santa Catarina',
+  sp: 'São Paulo',
+  se: 'Sergipe',
+  to: 'Tocantins',
+}
+Object.keys(statsList)
+
+function createStates() {
+  for (let index in statsList){
+    let newOption = document.createElement('option');
+    newOption.value = index;
+    newOption.innerText = statsList[index];
+    states.appendChild(newOption);
+  }
+}
+
+createStates()

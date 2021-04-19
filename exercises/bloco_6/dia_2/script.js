5943c4fa736a361036cc49be34a046a08256c827
Alberto Candido <albertocandido98@gmail.com>
2021-04-18 22:26:23 -0300

    Finaliza estilizaçao com Bootstrap e validaçoes com Pikday(data) e popup-validation(e-mail)

--- exercises/bloco_6/dia_2/script.js
+++ exercises/bloco_6/dia_2/script.js
@@ -1,5 +1,6 @@
 let states = document.querySelector('#brasil-states');
 let statsList = {
+  sl: 'Selecione seu estado',
   ac: 'Acre',
   al: 'Alagoas',
   ap: 'Amapá',
@@ -41,27 +42,14 @@ function createStates() {
 
 createStates()
 
-// 2 Validação da data
-
-let dateField = document.querySelector('#date-beginning');
-
-function validateDate() {
-  let date = dateField.split(' / ');
-  console.log(date);
-}
-
-validateDate()
-
-// 3
-
-// let submitButton = document.querySelector('#submit-button');
-// let formulario = document.querySelector('#formulario');
-
-// submitButton.addEventListener('click', (evt) => {
-//   evt.preventDefault();
-//   let newDiv = document.createElement('div');
-//   newDiv.innerText = formulario.innerText;
-//   document.body.appendChild(newDiv);
-// })
+// var picker = new Pikaday({ field: document.getElementById('datepicker') });
 
 
+var picker = new Pikaday({
+    field: document.getElementById('datepicker'),
+    format: 'DD/MM/YYYY',
+});
+
+validation.init("#formulario", {
+  events: ["change", "paste", "keyup"]
+});

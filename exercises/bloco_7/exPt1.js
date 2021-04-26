c6e18526c190fee04e88683268281ea71eadea3b
Alberto Candido <albertocandido98@gmail.com>
2021-04-26 18:30:27 -0300

    Dia 7.1 - Pt1 exercicio 1

---
+++ exercises/bloco_7/exPt1.js
@@ -0,0 +1,21 @@
+// const testingScope = escopo => {
+//   if (escopo === true) {
+//     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+//     console.log(ifScope);
+//   } else {
+//     let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//     console.log(elseScope);
+//   }
+//   // console.log(`${ifScope} o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
+// }
+
+// testingScope(true);
+
+// 2
+
+const oddsAndEvens = arr => arr.sort();
+
+// Seu código aqui.
+
+console.log(`Os números ${oddsAndEvens([13, 3, 4, 10, 7, 2])} se encontram ordenados de forma crescente!`);

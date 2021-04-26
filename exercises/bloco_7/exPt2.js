9048f7618837e552ff5acf40937a7aafa0f24b24
Alberto Candido <albertocandido98@gmail.com>
2021-04-26 19:11:28 -0300

    Finaliza ex 2 - pt 2

--- exercises/bloco_7/exPt2.js
+++ exercises/bloco_7/exPt2.js
@@ -15,11 +15,15 @@
 const longestWord = (frase) => {
   let arr = frase.split(' ');
   let biggerWord = '';
-  for (let index = 0; index < arr.length; index++) {
+  for (let index = 0; index < arr.length; index +=1) {
     if (arr[index].length > biggerWord.length) {
       biggerWord = arr[index]
     }
   }
+  return biggerWord
 }
 
 console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu'));
+
+// 3
+

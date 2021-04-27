b52668b369516b6615460439405a93ea71d05dd6
Alberto Candido <albertocandido98@gmail.com>
2021-04-27 19:30:46 -0300

    Resolve ex5 e ex6 - pt II

--- exercises/bloco_7/dia_2/exPt2.js
+++ exercises/bloco_7/dia_2/exPt2.js
@@ -22,9 +22,9 @@ const lesson3 = {
 // 1
 // Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
 
-// const addParam = (obj, key, value) => obj[key] = value;
+const addParam = (obj, key, value) => obj[key] = value;
 
-// addParam(lesson2, 'turno', 'manhã')
+addParam(lesson2, 'turno', 'manhã')
 
 // console.table(lesson2);
 
@@ -45,6 +45,32 @@ const lesson3 = {
 //4
 // Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
 
-const keysOfObj = (obj) => Object.values(obj);
+// const keysOfObj = (obj) => Object.values(obj);
 
-console.log(keysOfObj(lesson2));
+// console.log(keysOfObj(lesson2));
+
+//5
+// Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 .
+
+const allLessons = Object.assign({}, {lesson1}, {lesson2}, {lesson3})
+
+// console.log(allLessons);
+
+
+//6
+// Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+
+const sumStudents = (obj) => {
+  let lessons = Object.keys(obj);
+  sum = 0;
+  for (let index = 0; index < lessons.length; index += 1) {
+    sum += obj[lessons[index]].numeroEstudantes;
+  }
+  return sum;
+}
+
+console.log(sumStudents(allLessons));
+
+//7
+// Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto.
+

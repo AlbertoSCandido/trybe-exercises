6a41218cfe0d9b84c8454b662bf1051402a694c0
Alberto Candido <albertocandido98@gmail.com>
2021-04-26 21:43:53 -0300

    Finaliza exercicios 3 e 4 - pt 2

--- exercises/bloco_7/exPt2.js
+++ exercises/bloco_7/exPt2.js
@@ -1,4 +1,4 @@
-// 1
+// // 1
 
 // const fatorial = num1 => {
 //   let sum = 1;
@@ -10,20 +10,59 @@
 
 // console.log(fatorial(4));
 
-//2 
+// //2 
 
-const longestWord = (frase) => {
-  let arr = frase.split(' ');
-  let biggerWord = '';
-  for (let index = 0; index < arr.length; index +=1) {
-    if (arr[index].length > biggerWord.length) {
-      biggerWord = arr[index]
-    }
-  }
-  return biggerWord
+// const longestWord = (frase) => {
+//   let arr = frase.split(' ');
+//   let biggerWord = '';
+//   for (let index = 0; index < arr.length; index +=1) {
+//     if (arr[index].length > biggerWord.length) {
+//       biggerWord = arr[index]
+//     }
+//   }
+//   return biggerWord
+// }
+
+// console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu'));
+
+// // 3
+
+// let btn = document.getElementById('click-counter');
+// let counter = document.getElementById('counter');
+// let x = 0;
+
+// btn.addEventListener('click', () => {
+//   x +=1;
+//   counter.innerText = x;
+// })
+
+// 4;
+
+const replaceXString = (param1, param2) => {
+  return param1.replace('x', param2);
 }
 
-console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu'));
+const array = ["CSS", "HTML", "JS", "REACT", "NODE.JS"]
 
-// 3
+function buildSkillsPhrase () {
+  const func1 = replaceXString(`Tryber x aqui!
+  
+  Tudo bem?`,"Alberto");
 
+  let result = `${func1}
+
+  Minhas cinco principais habilidades são:`
+
+  array.forEach((skill) =>
+  result += `
+
+  - ${skill}`)
+
+  result += `
+
+  #goTrybe`
+
+  return result
+}
+
+console.log(buildSkillsPhrase());

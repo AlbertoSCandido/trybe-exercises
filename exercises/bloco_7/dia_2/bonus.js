21a00f2cc80958c3319c8e4e85aa9d2f2890cad4
Alberto Candido <albertocandido98@gmail.com>
2021-04-27 21:52:43 -0300

    Finaliza ex2 - Bonus

--- exercises/bloco_7/dia_2/bonus.js
+++ exercises/bloco_7/dia_2/bonus.js
@@ -22,15 +22,44 @@ let allLessons = {
 // 1
 // Crie uma função para contar quantos estudantes assistiram às aulas de Matemática. Use o objeto criado no exercício 5.
 
-const sumStudents = (obj) => {
+// const sumStudents = (obj) => {
+//   let lessons = Object.keys(obj);
+//   sum = 0;
+//   for (let index = 0; index < lessons.length; index += 1) {
+//     if (obj[lessons[index]].materia === 'Matemática') {
+//       sum += obj[lessons[index]].numeroEstudantes
+//     }
+//   }
+//   return sum;
+// }
+
+// console.log(sumStudents(allLessons));
+
+//2
+// Crie uma função que deverá retornar um objeto que representa o relatório do professor ou professora, as aulas que ele ou ela ministrou e o número total de estudantes.
+
+/* {
+  professor: 'Maria Clara',
+  aulas: [ 'Matemática', 'Matemática' ],
+  estudantes: 30
+} */
+
+let createReport = (obj, teacher) => {
+  let materias = [];
+  let estudantes = 0;
   let lessons = Object.keys(obj);
-  sum = 0;
-  for (let index = 0; index < lessons.length; index += 1) {
-    if (obj[lessons[index]].materia === 'Matemática') {
-      sum += obj[lessons[index]].numeroEstudantes
+  for (let index = 0; index < lessons.length; index +=1) {
+    if (obj[lessons[index]].professor === teacher) {
+      materias.push(obj[lessons[index]].materia);
+      estudantes += obj[lessons[index]].numeroEstudantes;
     }
   }
-  return sum;
+  return {
+    professor: teacher,
+    aulas: materias,
+    estudantes: estudantes,
+  };
+
 }
 
-console.log(sumStudents(allLessons));
+console.log(createReport(allLessons, 'Maria Clara'));

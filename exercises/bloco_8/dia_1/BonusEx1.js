5090b4b3e2857c8147d8d794b1a730f3a7330de4
Alberto Candido <albertocandido98@gmail.com>
2021-04-30 18:30:22 -0300

    Finaliza exercicio bonus 1 - cria funçao que retorna dano causado pelo dragao

---
+++ exercises/bloco_8/dia_1/BonusEx1.js
@@ -0,0 +1,29 @@
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
+
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+
+const battleMembers = { mage, warrior, dragon };
+
+const dragonDamage = () => {
+  let min = 15;
+  let dano = Math.ceil(Math.random() * 50);
+  return dano >= min ? dano : min;
+}
+
+console.log(dragonDamage());

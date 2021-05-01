d8ed668fe6a42d7b75fb9d1da594594f551814f8
Alberto Candido <albertocandido98@gmail.com>
2021-04-30 22:30:23 -0300

    Finaliza parte 2 bonus - todos os exercicios

--- exercises/bloco_8/dia_1/pt2ex1.js
+++ exercises/bloco_8/dia_1/pt2ex1.js
@@ -28,7 +28,7 @@ const dragonDamage = () => {
   return dano >= min ? dano : min;
 }
 
-console.log(dragonDamage());
+// console.log(dragonDamage());
 
 // Crie uma função que retorna o dano causado pelo warrior .
 
@@ -39,12 +39,11 @@ const warriorDamage = () => {
   return dano > min ? dano : min
 }
 
-console.log(warriorDamage());
+// console.log(warriorDamage());
 
 // Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
 // O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
 // A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de dano recebe uma mensagem (Ex: "Não possui mana suficiente") e a mana gasta é 0.
-
 const mageDamage = () => {
   let dano = Math.ceil(Math.random() * mage.intelligence * 2);
   let mana = mage.mana - 15;
@@ -54,4 +53,32 @@ const mageDamage = () => {
   return dano > mage.intelligence ? {damage:dano, mana: - 15} : {damage: mage.intelligence, mana: -15};
 }
 
-console.log(mageDamage());
+// console.log(mageDamage());
+
+
+const gameActions = {
+  turnWarriorXDragon: (block) => {
+    let dano = block();
+    warrior.damage = dano;
+    dragon.healthPoints -= dano;
+  },
+  turnMageXDragon: (block) => {
+    let dano = block();
+    dragon.healthPoints -= dano.damage;
+    mage.damage = dano.damage;
+    mage.mana = mage.mana;
+  },
+  turnDragonXMageAndWarrior: (block) => {
+    let dano = block();
+    mage.healthPoints -= dano;
+    warrior.healthPoints -= dano;
+    dragon.damage = dano;
+  },
+  battleMembers: { mage, dragon, warrior },
+};
+
+gameActions.turnWarriorXDragon(warriorDamage);
+gameActions.turnMageXDragon(mageDamage);
+gameActions.turnDragonXMageAndWarrior(dragonDamage)
+console.table(gameActions.battleMembers);
+

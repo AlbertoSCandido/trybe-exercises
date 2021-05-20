b0793f5365296a7831be5fed50ad1867584fdc06
Alberto Candido <albertocandido98@gmail.com>
2021-05-20 20:56:24 -0300

    Finaliza exercicio 4

---
+++ exercises/bloco_10/dia_3/exercise4.js
@@ -0,0 +1,9 @@
+// Dentro de um mesmo arquivo, crie três funções. A primeira deve receber uma string e retorná-la em caixa alta. A segunda deve também receber uma string e retornar só a primeira letra. A terceira deve receber duas strings e concatená-las. Faça o mock do arquivo. Faça uma nova implementação para a primeira função, mas agora ela deve retornar a string em caixa baixa. Para a segunda função, uma nova implementação deve retornar a última letra de uma string. A terceira deve receber três strings e concatená-las.
+
+const upperCase = (str) => str.toUpperCase();
+
+const getFirstLetter = (str) => str[0];
+
+const concatStr = (fStr, sStr) => fStr + sStr;
+
+module.exports = { upperCase, getFirstLetter, concatStr }

d6cf4c78fe57fa195e3316ab11c7e637e341f433
Alberto Candido <albertocandido98@gmail.com>
2021-09-23 18:27:19 -0300

    Adiciona exercicio 5

--- exercises/back-end/bloco_26/dia_1/my-scripts/imc.js
+++ exercises/back-end/bloco_26/dia_1/my-scripts/imc.js
@@ -4,7 +4,23 @@ function calcIMC() {
   const peso = readline.questionInt('Qual o seu peso?');
   const altura = readline.questionFloat('Qual a sua altura?');
   const IMC = peso / Math.pow(altura, 2);
-  return IMC;
+  let mensagem = '';
+
+  if(IMC < 18.5) {
+    mensagem = 'Abaixo do peso (magreza)'
+  } else if(IMC < 24,9) {
+    mensagem = 'Peso normal';
+  } else if(IMC < 29,9) {
+    mensagem = 'Acima do peso (sobrepeso)';
+  } else if(IMC < 34,9) {
+    mensagem = 'Obesidade grau I';
+  } else if (IMC < 39,9) {
+    mensagem = 'Obesidade grau II';
+  } else {
+    mensagem = 'Obesidade graus III e IV';
+  }
+
+  return `IMC = ${IMC} - ${mensagem}`;
 }
 
 console.log(calcIMC());

a37ffc89dee74383999a3a3eacae0b828dc25d38
Alberto Candido <albertocandido98@gmail.com>
2021-09-27 00:46:49 -0300

    Adiciona exercicios 2 e 3

--- exercises/back-end/bloco_26/dia_2/i0-local/exercises.js
+++ exercises/back-end/bloco_26/dia_2/i0-local/exercises.js
@@ -3,7 +3,6 @@
 // Caso todos os parâmetros sejam numéricos, some os dois primeiros e multiplique o resultado pelo terceiro ( (a + b) * c ).
 // Caso o resultado seja menor que 50, rejeite a Promise com o motivo "Valor muito baixo"
 // Caso o resultado seja maior que 50, resolva a Promise com o valor obtido.
-
 function exerciseOne(n1, n2, n3) {
   const promise = new Promise((resolve, reject) => {
     if(isNaN(n1) || isNaN(n2) || isNaN(n3)) {
@@ -15,9 +14,51 @@ function exerciseOne(n1, n2, n3) {
 
     resolve(result);
   });
+
   return promise;
+
 }
 
-exerciseOne(2, 2, 10)
-  .then(result => console.log(result))
-  .catch(error => console.log(error.message))
+// exerciseOne(2, 2, 10)
+//   .then(result => console.log(result))
+//   .catch(error => console.log(error.message))
+
+
+// Escreva um código para consumir a função construída no exercício anterior.
+// Gere um número aleatório de 1 a 100 para cada parâmetro que a função recebe. Para gerar um número aleatório, utilize o seguinte trecho de código: 
+// Math.floor(Math.random() * 100 + 1) .
+// Chame a função do exercício anterior, passando os três números aleatórios como parâmetros.
+// Utilize then e catch para manipular a Promise retornada pela função:
+// Caso a Promise seja rejeitada, escreva na tela o motivo da rejeição.
+// Caso a Promise seja resolvida, escreva na tela o resultado do cálculo.
+
+// function exerciseTwo() {
+//   const n1 = Math.floor(Math.random() * 100 + 1);
+//   const n2 = Math.floor(Math.random() * 100 + 1);
+//   const n3 = Math.floor(Math.random() * 100 + 1);
+
+//   return exerciseOne(n1, n2, n3)
+// }
+
+// exerciseTwo()
+//   .then(result => console.log(result))
+//   .catch(error => console.log(error.message))
+
+
+// Reescreva o código do exercício anterior para que utilize async/await .
+// Lembre-se: a palavra chave await só pode ser utilizada dentro de funções async .
+
+async function exerciseThree() {
+  const n1 = Math.floor(Math.random() * 10 + 1);
+  const n2 = Math.floor(Math.random() * 10 + 1);
+  const n3 = Math.floor(Math.random() * 10 + 1);
+
+  try {
+    const result = await exerciseOne(n1, n2, n3)
+    console.log(result);
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+exerciseThree();

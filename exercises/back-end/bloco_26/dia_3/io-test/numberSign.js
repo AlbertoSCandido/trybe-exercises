6827595c41a3135d2c2fd471d7ef6a2eae1386e0
Alberto Candido <albertocandido98@gmail.com>
2021-09-28 21:08:13 -0300

    Finaliza exercicios dia 26.3

--- exercises/back-end/bloco_26/dia_3/io-test/numberSign.js
+++ exercises/back-end/bloco_26/dia_3/io-test/numberSign.js
@@ -1,9 +1,12 @@
 // Exercício 2 : Implemente a função apresentada no exercício 1, garantindo que ela irá passar em todos os testes que você escreveu.
 
 function numberSign(n) {
-  if(n === 0) return 'neutro';
+  if(typeof n !== 'number') return 'o valor deve ser um número'
   if (n > 0) return 'positivo';
   if(n < 0) return 'negativo';
+  if(n === 0) return 'neutro';
 }
+
+console.log(numberSign('0'));
 
 module.exports = numberSign;
--- exercises/back-end/bloco_26/dia_3/io-test/test.js
+++ exercises/back-end/bloco_26/dia_3/io-test/test.js
@@ -4,6 +4,7 @@ const { expect } = require('chai');
 
 const numberSign = require('./numberSign');
 const leArquivo = require('./leArquivo');
+const writeFile = require('./writeFile');
 
 const CONTEUDO_DO_ARQUIVO = 'VQV com TDD';
 
@@ -60,6 +61,11 @@ describe('leArquivo', () => {
 // Descreva todos os testes que serão feitos utilizando its ;
 // Crie as asserções validando se os retornos de cada cenário tem o tipo e o valor esperado.
 
+// Exercício 3 Adicione à função um tratamento para casos em que o parâmetro informado não seja do tipo Number .
+// Adicione o cenário em seu arquivo de testes, passando um valor de tipo diferente a Number para a função;
+// Adicione uma asserção para esperar que o valor retornado para esse caso seja igual uma string "o valor deve ser um número";
+// Implemente em sua função tal validação para que o teste passe.
+
 describe('numberSign', () => {
   describe('quando 0 ', () => {
     it('deve retornar neutro', () => {
@@ -76,6 +82,44 @@ describe('numberSign', () => {
   describe('quando > 0', () => {
     it('deve retornar negativo', () => {
       expect(numberSign(-1)).to.be.equal('negativo')
+    });
+  });
+
+  describe('quando o argumento recebido não é um número', () => {
+    it('deve retornar - o valor deve ser um número', () => {
+      expect(numberSign('0')).to.be.equal('o valor deve ser um número');
+    });
+  });
+});
+
+// Exercício 4 Crie testes para uma função que escreverá um conteúdo em um arquivo específico.
+// Essa função deverá receber dois parâmetros: o nome do arquivo e o conteúdo desse arquivo.
+// Após concluir a escrita do arquivo ela deverá retornar um ok .
+// Descreva todos os cenários de teste utilizando describes ;
+// Descreva todos os testes que serão feitos utilizando its ;
+// Crie as asserções validando se o retorno da função possui o valor e tipo esperado.
+
+
+describe('writeFile', () => {  
+  describe('a resposta', () => {
+    before(() => {
+      sinon.stub(fs, 'writeFile');
+    });
+  
+    after(() => {
+      fs.writeFile.restore();
+    });
+
+    it('é uma "string"', async () => {
+      const resposta = await writeFile('arquivo.txt', '#vqv conteúdo');
+
+      expect(resposta).to.be.a('string');
+    });
+
+    it('é igual a "ok"', async () => {
+      const resposta = await writeFile('arquivo.txt', '#vqv conteúdo');
+
+      expect(resposta).to.be.equals('ok');
     });
   });
 });

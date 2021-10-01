daae6754ba46082f0fcf9feb2bec099dbe7e0f03
Alberto Candido <albertocandido98@gmail.com>
2021-10-01 11:43:35 -0300

    Adiciona exercicio 3

--- exercises/back-end/bloco_26/dia_4/exercises-express/index.js
+++ exercises/back-end/bloco_26/dia_4/exercises-express/index.js
@@ -8,9 +8,17 @@ app.get('/ping', (req, res) => {
 
 app.post('/hello/:name', (req, res) => {
   const { name } = req.params;
-
-  res.status(200).json({ message: `Hello, ${name}`});
+  
+  res.status(200).json({ message: `Hello, ${name}!`});
 })
+
+app.post('/greetings/:name/:age', (req, res) => {
+  const { name, age } = req.params;
+
+  if(Number(age) > 17) return res.status(200).json({ message: `Hello, ${name}!`});
+
+  res.status(401).json({ message: 'Unauthorized'});
+});
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');

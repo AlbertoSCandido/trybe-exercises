d3ba7a09e08c6e4e3a96d6f76627093aaf41766e
Alberto Candido <albertocandido98@gmail.com>
2021-10-04 16:18:08 -0300

    Adiciona exercicio 2

--- exercises/back-end/bloco_26/dia_5/exercicios/index.js
+++ exercises/back-end/bloco_26/dia_5/exercicios/index.js
@@ -1,23 +1,37 @@
 const express = require('express');
+const crypto = require('crypto');
 
 const app = express();
 
 app.use(express.json());
 
-
-app.post('/user/register', (req, res) => {
-  const { username, email, password } = req.body;
+function validateEmailAndPassword(req, res, next) {
+  const { email, password } = req.body;
 
   const expEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  if(!email || !password || !username || username.length <= 3 || !expEmail.test(email) || password.length < 4 || password.length > 8) {
+  if(!email || !password || !expEmail.test(email) || password.length < 4 || password.length > 8) {
+    return res.status(400).json({ message: 'email or password is incorrect'});
+  }
+
+  next()
+}
+
+app.post('/user/register', validateEmailAndPassword, (req, res) => {
+  const { username} = req.body;
+
+  if(!username || username.length <= 3) {
     return res.status(400).json({ message: 'ivalid data'});
   }
 
   res.status(200).json({ message: 'user created' });
 });
 
+app.post('/user/login', validateEmailAndPassword, (_req, res) => {
+  const randomTokem = crypto.randomBytes(16).toString('base64');
 
+  res.status(200).json({ "token": randomTokem });
+});
 
 
 app.listen(3001, () => {

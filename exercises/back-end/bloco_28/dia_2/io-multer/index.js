8cd45d840870eca2b005de26292e461fd0ba8dc8
Alberto Candido <albertocandido98@gmail.com>
2021-10-26 10:13:01 -0300

    Adiciona rota para envios de multiplos arquivos

--- exercises/back-end/bloco_28/dia_2/io-multer/index.js
+++ exercises/back-end/bloco_28/dia_2/io-multer/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const multer = require('multer');
 
 const { PORT } = process.env;
 
@@ -20,6 +21,29 @@ app.use(
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+/* Definindo nossa pasta pública */
+/* `app.use` com apenas um parâmetro quer dizer que
+   queremos aplicar esse middleware a todas as rotas, com qualquer método */
+/* __dirname + '/uploads' é o caminho da pasta que queremos expor publicamente */
+/* Isso quer dizer que, sempre que receber uma request, o express vai primeiro
+   verificar se o caminho da request é o nome de um arquivo que existe em `uploads`.
+   Se for, o express envia o conteúdo desse arquivo e encerra a response.
+   Caso contrário, ele chama `next` e permite que os demais endpoints funcionem */
+app.use(express.static(__dirname + '/uploads'));
+
+/* Cria uma instância do`multer`configurada. O`multer`recebe um objeto que,
+   nesse caso, contém o destino do arquivo enviado. */
+const upload = multer({ dest: 'uploads' });
+const envios = multer({ dest: 'envios' });
+
+app.post('/files/upload', upload.single('file'), (req, res) =>
+  res.status(200).json({ body: req.body, file: req.file })
+);
+
+app.post('/envios', envios.array('arquivos'), (req, res) => {
+  res.status(200).json({ body: req.body, files: req.files });
+});
 
 app.get('/ping', controllers.ping);
 

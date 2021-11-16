678e9c6efecd1b78a70cbf0c20468ded64d660a3
Alberto Candido <albertocandido98@gmail.com>
2021-11-16 18:34:57 -0300

    Adiciona exercicio 3

--- exercises/back-end/bloco_29/dia_2/exercises/index.js
+++ exercises/back-end/bloco_29/dia_2/exercises/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Funcionando');
+  res.send(`Variavel de ambiente NAME = ${process.env.ENVIRONMENT}`);
 });
 
 app.listen(PORT, () => {

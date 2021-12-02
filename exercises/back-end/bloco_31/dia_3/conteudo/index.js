c9cfa6bc16021af08061c007da6b2f11175d0e5c
Alberto Candido <albertocandido98@gmail.com>
2021-12-02 15:45:30 -0300

    Refatorando

--- exercises/back-end/bloco_31/dia_3/conteudo/index.js
+++ exercises/back-end/bloco_31/dia_3/conteudo/index.js
@@ -9,15 +9,10 @@ const io = require('socket.io')(http, {
   }}
 );
 
-io.on('connection', (socket) => {
-  socket.emit('ola', 'Que bom que você chegou aqui! Fica mais um cadin, vai ter bolo :)');
-  console.log(`Usuário conectado. ID: ${socket.id} `);
+app.use(express.static(__dirname + '/public'));
 
-  socket.on('ping', () => {
-    console.log(`${socket.id} emitiu um ping!`);
-    io.emit('pong', `${socket.id} enviou um ping!`); // essa linha envia um aviso para o cliente que o ping chegou.
-  });
-});
+require('./sockets/ping')(io);
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');

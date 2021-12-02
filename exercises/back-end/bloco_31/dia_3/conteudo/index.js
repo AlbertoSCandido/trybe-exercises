5e38665a91d22d6b6483a5f10924a018e47cbd2e
Alberto Candido <albertocandido98@gmail.com>
2021-12-02 15:21:59 -0300

    Abre instancia do socket.io com server http e objeto de options

--- exercises/back-end/bloco_31/dia_3/conteudo/index.js
+++ exercises/back-end/bloco_31/dia_3/conteudo/index.js
@@ -2,15 +2,15 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 
-// const io = require('socket.io')(http, {
-//   cors: {
-//     origin: 'http://localhost:3000', // url aceita pelo cors
-//     methods: ['GET', 'POST'], // Métodos aceitos pela url
-//   }});
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000', // url aceita pelo cors
+    methods: ['GET', 'POST'], // Métodos aceitos pela url
+  }});
 
-// io.on('connection', (socket) => {
-//   console.log(`Usuário conectado. ID: ${socket.id} `);
-// });
+io.on('connection', (socket) => {
+  console.log(`Usuário conectado. ID: ${socket.id} `);
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');

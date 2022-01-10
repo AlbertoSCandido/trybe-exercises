8e157479f179deaac274c1f4c948f45e5bb4bdc9
Alberto Candido <albertocandido98@gmail.com>
2022-01-09 21:57:51 -0300

    exercicios estruturas de repetiçao e compreensao de listas

--- exercises/CS/bloco_32/dia_1/conteudo.py
+++ exercises/CS/bloco_32/dia_1/conteudo.py
@@ -49,3 +49,29 @@ info["recorrente"] = "Sim"
 
 # Exercício 10: Remova a propriedade cuja chave é "origem" e imprima o objeto no console.
 del info["origem"]
+
+# Exercício 13: O Fatorial de um número inteiro é representado pela multiplicação de todos os números predecessores maiores que 0. Por exemplo o fatorial de 5 é 120 pois 5! = 1 * 2 * 3 * 4 * 5 . Escreva um código que calcule o fatorial de um número inteiro.
+number = 5
+counter = 1
+result = 1
+
+while counter <= number:
+    result = result * counter
+    counter += 1
+
+number = 5
+result = 1
+# Usamos number + 1 pro range ir até o number
+for factor in range(1, number+1):
+    result *= factor
+result
+
+# Exercício 14: Um sistema de avaliações registra valores de 0 a 10 para cada avaliação. Após algumas mudanças estes valores precisam ser ajustados para avaliações de 0 a 100. Dado uma sequência de avaliações ratings = [6, 8, 5, 9, 10] . Escreva um código capaz de gerar as avaliações após a mudança. Neste caso o resultado deveria ser [60, 80, 50, 90, 100] .
+ratings = [6, 8, 5, 9, 10]
+new_ratings = []
+
+for rating in ratings:
+    new_ratings.append(rating * 10)
+
+ratings = [6, 8, 5, 9, 10]
+new_ratings = [10 * rating for rating in ratings]

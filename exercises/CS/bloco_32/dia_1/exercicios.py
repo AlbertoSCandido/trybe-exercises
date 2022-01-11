87740cff6274dc91ca108d05277bc65f0b380ae5
Alberto Candido <albertocandido98@gmail.com>
2022-01-11 20:42:41 -0300

    Finaliza exercicios dia 32.1

--- exercises/CS/bloco_32/dia_1/exercicios.py
+++ exercises/CS/bloco_32/dia_1/exercicios.py
@@ -87,3 +87,28 @@ def sum_numbers(n):
         sum += i + 1
     return sum
 
+
+# Exercício 4: Um posto está vendendo combustíveis com a seguinte tabela de descontos:
+  # Álcool:
+  #   - Até 20 litros, desconto de 3% por litro;
+  #   - Acima de 20 litros, desconto de 5% por litro.
+  # Gasolina:
+  #   - Até 20 litros, desconto de 4% por litro;
+  #   - Acima de 20 litros, desconto de 6% por litro.
+# Escreva uma função que receba o número de litros vendidos, o tipo de combustível (codificado da seguinte forma: A - álcool, G - gasolina), e retorne o valor a ser pago pelo cliente, sabendo-se que o preço do litro da gasolina é R$ 2,50, e o preço do litro do álcool é R$ 1,90.
+def price_gasoline(liters, type):
+    if type == 'A':
+        price = liters * 1.90
+        if (liters > 20):
+            price -= price * 0.05
+        else:
+            price -= price * 0.03
+    else:
+        price = liters * 2.50
+        if (liters > 20):
+            price -= price * 0.06
+        else:
+            price -= price * 0.04
+
+    return price
+

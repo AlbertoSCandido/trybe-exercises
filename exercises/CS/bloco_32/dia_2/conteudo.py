8f8bdff6954f607c341c4d8498b9eda021536d83
Alberto Candido <albertocandido98@gmail.com>
2022-01-17 21:27:08 -0300

    Adiciona exercicio 1 - conteudo

---
+++ exercises/CS/bloco_32/dia_2/conteudo.py
@@ -0,0 +1,8 @@
+# Exercício 1: Faça um programa que solicite o nome de uma pessoa usuária e imprima-o na vertical. Exemplo:
+
+def print_name():
+    name = input("Digite seu nome: ")
+    for letter in name:
+        print(letter)
+      
+print_name()

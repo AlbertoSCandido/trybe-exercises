66d06a247eb36e8fa154fb03e15432938161fd95
Alberto Candido <albertocandido98@gmail.com>
2022-01-25 20:45:02 -0300

    Adiciona metodo para printar dados de customer e cria testes para a classe

--- exercises/CS/bloco_33/dia_1/aula/customers.py
+++ exercises/CS/bloco_33/dia_1/aula/customers.py
@@ -1,11 +1,14 @@
 class Customer:
-	def __init__(self,name, email):
+	def __init__(self,name, email=""):
 		self.name = name
 		self.email = email
 		self._pin = "123"
 
-	def setPassword(self,password):
+	def setPassword(self, password):
 		self._pin = password
 	
-	def vericyPassword(self,password):
+	def vericyPassword(self, password):
 		return self._pin == password
+	
+	def show(self):
+		print(f"Cliente: {self.name}, email: {self.email}")
---
+++ exercises/CS/bloco_33/dia_1/aula/teste.py
@@ -0,0 +1,8 @@
+from customers import Customer
+
+cliente1 = Customer("João", "joaosousa@gmail.com")
+cliente1.show()
+
+cliente2 = Customer("Alberto")
+cliente2.email = "albertocandido@gmail.com"
+cliente2.show()

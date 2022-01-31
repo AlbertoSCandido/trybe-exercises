dd72acc395ebee8f2cb6c73607ff80a27bbb4a2a
Alberto Candido <albertocandido98@gmail.com>
2022-01-31 19:28:10 -0300

    Conteudo dia 33.3 - padroes de projeto - iterator

---
+++ exercises/CS/bloco_33/dia_3/itarator.py
@@ -0,0 +1,7 @@
+lista = [1, 2, 3, 4]
+
+lista_iterator = lista.__iter__()
+print(lista_iterator.__next__())
+print(lista_iterator.__next__())
+print(lista_iterator.__next__())
+print(lista_iterator.__next__())

daaf78e1a18e1f8432c4528dbdb0a7072babd60d
Alberto Candido <albertocandido98@gmail.com>
2022-02-14 21:43:09 -0300

    complexidade quadratica - O(n^3)

--- exercises/CS/bloco_35/dia_1/conteudo.py
+++ exercises/CS/bloco_35/dia_1/conteudo.py
@@ -27,3 +27,26 @@ def multiply_arrays(array1, array2):
 meu_array = [1,2,3,4,5]
 
 multiply_arrays(meu_array, meu_array)
+
+
+# Exercício 2: Para desembaraçar a sopa de letrinhas que a seção anterior criou, meça o tempo de execução do algoritmo acima e, mudando o tamanho das entradas, veja como, se você aumenta a entrada em n vezes, o tempo de execução aumenta em n² vezes!
+
+
+# Exercício 3: Faça um algoritmo qualquer com três loops aninhados um dentro do outro. Entenda como ele terá uma complexidade de O(n³) !
+def multiply_arrays(array1, array2, array3):
+    result = []
+    number_of_iterations = 0
+
+    for number1 in array1:
+        for number2 in array2:
+            for number3 in array3:
+                result.append(number1 * number2 * number3)
+                number_of_iterations += 1
+
+    print(f'{number_of_iterations} iterações!')
+    return result
+
+
+# Usar 1000 aqui vai ser muito lento!
+meu_array = list(range(1, 200))
+multiply_arrays(meu_array, meu_array, meu_array)
